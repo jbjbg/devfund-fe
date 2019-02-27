fd7b2642e82e3918da4d3814badd8b681060dc3a
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2019-02-27 09:00:05 -0800

    Styles request page, adds images, removes extranneous files

--- src/components/pitch.js
+++ src/components/pitch.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {When} from './conditionals.js'
+import '../styles/reset.scss';
+import '../styles/base.scss';
+import '../styles/pitch.scss';
 
 class Pitch extends React.Component{
   constructor(props){
@@ -26,27 +29,34 @@ class Pitch extends React.Component{
   render() {
     return(
       <>
-      <form onSubmit={this.handleSubmit}>
-        <fieldset>
-          <span>Service Requested: </span>
-          <select name="yourrequest" value={this.state.value} onChange={this.handleChange} required>
-            <option value="default">Make a selection</option>
-            <option value="egghead">Egghead.io Membership</option>
-            <option value="udemy">Udemy.com Course</option>
-            <option value="plural">Pluralsight.com Membership</option>
-            <option value="other">Other</option>
-          </select>
-          <When condition={this.state.show}>
-            <span>Other: </span>
-            <input type="text" required></input>
-          </When>
-          <span>Cost: </span>
-          <input type="number" step=".01"></input>
-          <span>Your Pitch:</span>
-          <input type="textarea"></input>
-          <button type="submit">Submit</button>
-        </fieldset>
-      </form>
+      <main id="pitchPage">
+        <section class="hero" id="pitch">
+          <h1>Make a Request</h1>
+          <h3>DevFunders love to help!</h3>
+        </section>
+          <h2 id="formheader">What do you need DevFunded?</h2>
+        <form onSubmit={this.handleSubmit}>
+          <fieldset>
+            <label>Service Requested: </label>
+            <select name="yourrequest" value={this.state.value} onChange={this.handleChange} required>
+              <option value="default">Make a selection</option>
+              <option value="egghead">Egghead.io Membership</option>
+              <option value="udemy">Udemy.com Course</option>
+              <option value="plural">Pluralsight.com Membership</option>
+              <option value="other">Other</option>
+            </select>
+            <When condition={this.state.show}>
+              <label>Other: </label>
+              <input type="text" required></input>
+            </When>
+            <label>Cost: </label>
+            <input type="number" step=".01"></input>
+            <label>Your Pitch:</label>
+            <input type="textarea"></input>
+            <button type="submit">Submit</button>
+          </fieldset>
+        </form>
+      </main>
       </>
     )
   }
