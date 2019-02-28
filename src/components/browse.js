13d17f5127c5ff8aaf2d3b593897cecd3c4f0e3f
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2019-02-28 08:52:39 -0800

    starts styling browse page

--- src/components/browse.js
+++ src/components/browse.js
@@ -5,6 +5,10 @@ import {When} from "./conditionals.js";
 import Modal from './modules/modal.js';
 import Detail from './detail.js';
 
+import "../styles/reset.scss";
+import "../styles/base.scss";
+import "../styles/browse.scss";
+
 
 
 //limit characters on the Why - end with a ... to show more is available.
@@ -24,21 +28,28 @@ class Browse extends React.Component {
   render() {
     return(
       <>
-        <When condition={this.state.showModal}>
-          <Modal title="" close={this.toggleModal}>
-            <Detail/>
-          </Modal>
-        </When>
-        <ul>{pitches.data.map((pitch, i) => 
-          <li key={i}>
-          <img src={pitch.image} alt="profile pic" />
-          <p>{pitch.username}</p>
-          <h4>{pitch.item} - {pitch.cost}</h4>
-          <p>{pitch.why}</p>
-          <button onClick={this.toggleModal}>Click Me!</button>
-          </li> 
-          )}
-        </ul>
+        <main id="browsePage">
+          <section className="hero" id="browse">
+            <h1>DevFund a Request</h1>
+            <h3>You can make a big difference!</h3>
+          </section>
+          <h2>All Pitches</h2>
+          <When condition={this.state.showModal}>
+            <Modal title="" close={this.toggleModal}>
+              <Detail/>
+            </Modal>
+          </When>
+          <ul>{pitches.data.map((pitch, i) => 
+            <li key={i}>
+            <img src={pitch.image} alt="profile pic" />
+            <p>{pitch.username}</p>
+            <h4>{pitch.item} - {pitch.cost}</h4>
+            <p>{pitch.why}</p>
+            <button onClick={this.toggleModal}>Click Me!</button>
+            </li> 
+            )}
+          </ul>
+        </main>
       </>
     )}
 }
