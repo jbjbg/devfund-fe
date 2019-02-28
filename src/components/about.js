c04f5a0f9b31906ef582034b184a7d59b8a64737
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2019-02-28 12:30:27 -0800

    Styles about page

--- src/components/about.js
+++ src/components/about.js
@@ -18,47 +18,86 @@ const About = () => {
 
         <h2>Meet the Team</h2>
 
+        <section>
+
         <div>
-          <p>George Raymond</p>
-          <img src={require("../assets/people/george.jpeg")} alt="George" width="250" height="250"/>
-          <p>Bio goes here</p>
-          <p><a href="https://www.linkedin.com/in/george-raymond-8725b9152/">LinkeIn: in/george-raymond-8725b9152/</a></p>
-          <p><a href="https://github.com/georgeraymond92">GitHub: github.com/georgeraymond92</a></p>
+          <p className="name">George Raymond</p>
+          <img src={require("../assets/people/george.jpg")} alt="George"/>
+          <div className="social">
+            <a href="https://www.linkedin.com/in/george-raymond-8725b9152/">
+              <img className="socialicon" src={require("../assets/social/iconfinder_icon-social-linkedin-outline_211909.png")} alt="LinkedIn Icon"/>
+            </a>
+            <a href="https://github.com/georgeraymond92">
+                <img className="socialicon" src={require("../assets/social/iconfinder_icon-social-github-outline_211903.png")} alt="GitHub Icon"/>
+            </a>
+          </div>
+          <p>My name is George Raymond and I am a Software Developer and General Tech Enthusiast. As a recent transplant from Los Angeles I am enjoying networking and connecting with new people in Seattle who share my passion for technology. Outside of technology I enjoy exploring new restaurants, cooking, and spending time with my dog and family. doggo</p>
         </div>
 
         <div>
-          <p>Becca Lee</p>
-          <img src={require("../assets/people/becca.jpg")} alt="Becca" width="250" height="250"/>
-          <p>Becca is a software developer with a background in marketing and customer service, who fell in love with the tech industry while working as the marketing manager for a JavaScript consultancy. She is a voracious learner who loves to translate her knowledge and intuition into thoughtful, user-focused strategies. Becca recently graduated from the JavaScript program at Code Fellows and is excited to use her new skills to create user-centric apps and solve challenging software problems.</p>
-          <p><a href="https://twitter.com/the_becca_lee">Twitter: https://twitter.com/the_becca_lee</a></p>
-          <p><a href="https://www.linkedin.com/in/beccalee123/">LinkeIn: in/beccalee123/ </a></p>
-          <p><a href="https://github.com/beccalee123">GitHub: github.com/beccalee123</a></p>
+          <p className="name">Becca Lee</p>
+          <img src={require("../assets/people/becca.jpg")} alt="Becca"/>
+          <div className="social">
+            <a href="https://twitter.com/the_becca_lee">
+                <img className="socialicon" src={require("../assets/social/iconfinder_icon-social-twitter-outline_211919.png")} alt="GitHub Icon"/>
+            </a>
+            <a href="https://www.linkedin.com/in/beccalee123/">
+              <img className="socialicon" src={require("../assets/social/iconfinder_icon-social-linkedin-outline_211909.png")} alt="LinkedIn Icon"/>
+            </a>
+            <a href="https://github.com/beccalee123">
+                <img className="socialicon" src={require("../assets/social/iconfinder_icon-social-github-outline_211903.png")} alt="GitHub Icon"/>
+            </a>
+          </div>
+          <p>Becca is a software developer with a background in marketing and customer service, who fell in love with the tech industry while working as the marketing manager for a JavaScript consultancy. She is a voracious learner who loves to translate her knowledge and intuition into thoughtful, user-focused strategies.</p>
+          
         </div>
 
         <div>
-          <p>Jonathan DiQuattro</p>
-          <img src={require("../assets/people/jon.jpeg")} alt="Jon" width="250" height="250"/>
-
-          <p>A veteran and proven learner, Jonathan has a BA in Computer Science and is looking to make an impact as a fullstack/backend developer.</p>
-          <p><a href="https://www.linkedin.com/in/jonathandiquattro/">Linkedin: /in/jonathandiquattro</a></p>
-          <p><a href="https://github.com/jondiquattro">GitHub: github.com/jondiquattro</a></p>
+          <p className="name">Brent Woodward</p>
+          <img src={require("../assets/people/brent-2.jpg")} alt="Brent"/>
+          <div className="social">
+            <a href="https://www.linkedin.com/in/brenton-woodward/">
+                <img className="socialicon" src={require("../assets/social/iconfinder_icon-social-linkedin-outline_211909.png")} alt="LinkedIn Icon"/>
+            </a>
+            <a href="https://github.com/BrentTech">
+                <img className="socialicon" src={require("../assets/social/iconfinder_icon-social-github-outline_211903.png")} alt="GitHub Icon"/>
+            </a>
+          </div>
+          <p>Brent is a Software Developer currently studying at Code Fellows who loves a good puzzle. Originally from the Midwest, he longed to visit the ocean. Now that he lives in Washington, he can enjoy this delightful experience whenever he wants. When not hard at work learning software development, he enjoys spending time with his wife, reading, hiking, enjoying a fine cigar, and playing games of all kinds.</p>
         </div>
 
         <div>
-          <p>Brent Woodward</p>
-          <img src={require("../assets/people/brent-2.jpg")} alt="B-rent" width="250" height="250"/>
-          <p>Bio goes here</p>
-          <p><a href="https://www.linkedin.com/in/brenton-woodward/">LinkeIn: /in/brenton-woodward/</a></p>
-          <p><a href="https://github.com/BrentTech">GitHub: github.com/BrentTech </a></p>
-        </div>
-
-        <div>
-          <p>Jacob Anderson</p>
-          <img src={require("../assets/people/jake.jpeg")} alt="Yacob" width="250" height="250"/>
+          <p className="name">Jacob Anderson</p>
+          <img src={require("../assets/people/jake.jpeg")} alt="Jacob"/>
+          <div className="social">
+            <a href="https://www.linkedin.com/in/jacob-merrill-anderson/">
+                <img className="socialicon" src={require("../assets/social/iconfinder_icon-social-linkedin-outline_211909.png")} alt="LinkedIn Icon"/>
+            </a>
+            <a href="https://github.com/1anderson2jacob">
+                <img className="socialicon" src={require("../assets/social/iconfinder_icon-social-github-outline_211903.png")} alt="GitHub Icon"/>
+            </a>
+          </div>
           <p>Jake grew up on legos, little did he know they wouldn't just be the building blocks of the castle he was making but also the building blocks for problem solving skills and lego piece bartering skills. If there's one thing that puts the motion in this man's ocean commotion it's playing card games with friends.</p>
-          <p><a href="https://www.linkedin.com/in/jacob-merrill-anderson/">LinkeIn: /in/jacob-merrill-anderson/ </a></p>
-          <p><a href="https://github.com/1anderson2jacob">GitHub: github.com/1anderson2jacob </a></p>
         </div>
+
+        
+        <div>
+          <p className="name">Jonathan DiQuattro</p>
+          <img src={require("../assets/people/jon.jpeg")} alt="Jon"/>
+          <div className="social">
+            <a href="https://www.linkedin.com/in/jonathandiquattro/">
+                <img className="socialicon" src={require("../assets/social/iconfinder_icon-social-linkedin-outline_211909.png")} alt="LinkedIn Icon"/>
+            </a>
+            <a href="https://github.com/https://github.com/jondiquattro">
+                <img className="socialicon" src={require("../assets/social/iconfinder_icon-social-github-outline_211903.png")} alt="GitHub Icon"/>
+            </a>
+          </div>
+          <p>A veteran and proven learner, Jonathan has a BA in Computer Science and is looking to make an impact as a fullstack/backend developer.</p>
+
+        </div>
+
+
+        </section>
       </main>
     </>
   )
